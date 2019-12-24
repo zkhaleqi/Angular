9f858c89fde9ce1b0392f83b2664c2fa4a06bb47
zkhaleqi <36332955+zkhaleqi@users.noreply.github.com>
2019-12-24 19:40:06 +0100

    Add pie chart component data

--- angularDontetDashboard/src/app/charts/pie-chart/pie-chart.component.ts
+++ angularDontetDashboard/src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,14 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData: number[] = [350, 450, 500];
+  pieChartLables: string[] = ['XYZ Company', 'Main St Logistics', 'Sample Company'];
+  colors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166']
+    }
+  ];
+  pieChartType = 'pie';
   ngOnInit() {
   }
 
