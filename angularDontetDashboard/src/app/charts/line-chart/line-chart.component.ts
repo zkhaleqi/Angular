6953bdd39cec74fd0a6a758708b488345ba86784
zkhaleqi <36332955+zkhaleqi@users.noreply.github.com>
2019-12-24 20:54:51 +0100

    add line chart

--- angularDontetDashboard/src/app/charts/line-chart/line-chart.component.ts
+++ angularDontetDashboard/src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.colors';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data: [32, 25, 39, 55, 21, 12], label: 'Sentiment Analysis' },
+  { data: [54, 55, 12, 15, 56, 70], label: 'Image Recognition' },
+  { data: [20, 35, 49, 77, 15, 10], label: 'Forecasting' },
+];
+
+const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul'];
+
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +18,15 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true
+  };
+  lineChartLegend = true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_COLORS;
 
   ngOnInit() {
   }
