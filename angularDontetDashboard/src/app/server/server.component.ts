e88de91c748dd149b5868f2f8a76be1a448df350
zkhaleqi <36332955+zkhaleqi@users.noreply.github.com>
2019-12-25 02:49:37 +0100

    updates server component

--- angularDontetDashboard/src/app/server/server.component.ts
+++ angularDontetDashboard/src/app/server/server.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Server } from '../shared/server';
+import { isBoolean } from 'util';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
@@ -9,8 +10,27 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText: string;
+
   @Input() serverInput: Server;
 
   ngOnInit() {
+    this.setServerStaut(this.serverInput.isOnline)
+  }
+  setServerStaut(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+  }
+  toggleStauts(onlineStatus: boolean) {
+    console.log(this.serverInput.name, ': ', onlineStatus);
+    this.setServerStaut(!onlineStatus)
   }
 }
