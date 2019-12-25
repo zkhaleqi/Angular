86804cef64be934648ef0edf1f4f8898080e9b3b
zkhaleqi <36332955+zkhaleqi@users.noreply.github.com>
2019-12-25 02:28:16 +0100

    Creates server component

--- angularDontetDashboard/src/app/sections/section-orders/section-health/section-health.component.ts
+++ angularDontetDashboard/src/app/sections/section-orders/section-health/section-health.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Server } from '../../../shared/server';
+
+const SAMPLE_SERVERS = [
+  { id: 1, name: 'dev-web', isOnline: false },
+  { id: 1, name: 'dev-email', isOnline: true },
+  { id: 1, name: 'prod-web', isOnline: true },
+  { id: 1, name: 'prod-email', isOnline: true }
+];
 
 @Component({
   selector: 'app-section-health',
@@ -9,7 +17,10 @@ export class SectionHealthComponent implements OnInit {
 
   constructor() { }
 
+  servers: Server[] = SAMPLE_SERVERS;
+
   ngOnInit() {
   }
 
 }
+
