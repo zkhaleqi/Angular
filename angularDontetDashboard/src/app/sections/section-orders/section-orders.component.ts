68794e23429f22db314052e3dbbec670e34f9b9f
zkhaleqi <36332955+zkhaleqi@users.noreply.github.com>
2019-12-25 01:58:43 +0100

    add orders section

--- angularDontetDashboard/src/app/sections/section-orders/section-orders.component.ts
+++ angularDontetDashboard/src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +9,36 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    {
+      id: 1,
+      customer:
+        { id: 1, name: 'Main St Bakery', state: 'CO', email: 'info@bakery.com' },
+      total: 230,
+      placed: new Date(2019, 12, 1),
+      fulfilled: new Date(2019, 12, 2),
+      status: 'Completed'
+    },
+    {
+      id: 2,
+      customer:
+        { id: 1, name: 'Main St Bakery', state: 'CO', email: 'info@bakery.com' },
+      total: 230,
+      placed: new Date(2019, 12, 1),
+      fulfilled: new Date(2019, 12, 2),
+      status: 'Completed'
+    },
+    {
+      id: 3,
+      customer:
+        { id: 1, name: 'Main St Bakery', state: 'CO', email: 'info@bakery.com' },
+      total: 230,
+      placed: new Date(2019, 12, 1),
+      fulfilled: new Date(2019, 12, 2),
+      status: 'Completed'
+    },
+  ];
 
   ngOnInit() {
   }
